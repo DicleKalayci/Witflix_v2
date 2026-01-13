bc2f7aa78186345d90d231789018fd16558f2c8a
DicleKalayci <diclesavasci@gmail.com>
2026-01-13 14:28:27 +0300

    signin page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import Welcome from './pages/Welcome'
 import Browse from './pages/Browse'
@@ -9,6 +9,7 @@ import { useState } from 'react'
 
 function App() {
   const [activeProfile, setActiveProfile] = useState(null);
+  const [user, setUser] = useState(null);
 
   return (
     <>
@@ -17,13 +18,19 @@ function App() {
         <Redirect to="/signup" />
       </Route>
       <Route path="/signup">
-        <SignUp />
+        <SignUp setUser={setUser}/>
       </Route>
       <Route path="/welcome">
-        <Welcome setActiveProfile={setActiveProfile}/>
+      {user ? 
+        (<Welcome setActiveProfile={setActiveProfile}/>) : (
+        <Redirect to="/signup" />
+        )}
       </Route>
       <Route path="/browse">
-        <Browse activeProfile={activeProfile}/>
+        {user ? 
+        (<Browse activeProfile={activeProfile}/>) : (
+        <Redirect to="/signup" />
+        )}
       </Route>
     
     </Switch>
@@ -33,4 +40,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,154 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import logo from '../assets/logo.png';
+import styled from 'styled-components';
+import hero from '../assets/hero_image.png';
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
 
-function SignUp() {
+const Hero = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    background-size: cover;
+    background-image: url(${hero});
+    height: 100vh;
+    width: 100vw;
+    
+`;
+
+const Header = styled.header`
+    width: 80%;
+    margin: auto;
+    
+`;
+
+const Logo = styled.img`
+    height: 2.5rem;
+    margin: 2rem 0;
+    display: block;
+`;
+
+const Form = styled.form`
+    width: 450px;
+    padding: 3rem 4rem;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    box-sizing: border-box;
+    background-color: rgba(0, 0, 0, 0.75);
+    color: white;
+`;
+
+const Input = styled.input`
+    padding: 1.2rem;
+    border-radius: 0.5rem;
+    border: 0;
+    background-color: rgba(0, 0, 0, 0.75);
+    color: white;
+    font-size: 1rem;
+`;    
+
+const Button = styled.button`
+    background: red;
+    &:disabled {
+        background: gray;
+    }
+`;
+
+const ErrorMessage = styled.p`
+    color: red;
+`;
+
+function SignUp(props) {
+    const { setUser } = props;
+
+    const history = useHistory();
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const [errors, setErrors] = useState({
+        email: "",
+        password: ""
+
+    });
+
+    const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+       if(validateEmail(formData.email) && validateStrongPassword(formData.password)) {
+        setIsValid(true)
+       } else {
+        setIsValid(false)
+       }
+    }, [formData])
+
+    const validateEmail = (email) => {
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+    const validateStrongPassword = (password) => {
+        let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+         return regex.test(password);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.post("https://jsonplaceholder.typicode.com/users", formData)
+        .then((response) => {
+            setUser(response.data);
+            history.push("/welcome");
+        })
+        .catch((error) => 
+            console.log(error.message)
+        )
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+        const newFormData = {...formData, [name]: value};
+        setFormData(newFormData);
+
+        if (name === "email") {
+            if (validateEmail(value)) {
+               setErrors({...errors, email: ""})
+            } else {
+               setErrors({...errors, email: "Geçerli bir email adresi giriniz."});
+            }
+    }
+
+     if (name === "password") {
+            if (validateStrongPassword(value)) {
+               setErrors({...errors, password: ""})
+            } else {
+               setErrors({...errors, password: "Güçlü bir şifre giriniz."});
+            }
+    }
+}
+
+
+
     return (
-        <div>SignUp</div>
+        <Hero>
+        <Header>
+            <Logo src={logo} />
+        </Header>
+        <Form onSubmit={handleSubmit}>
+            <h1>Sign In</h1>
+            <Input name="email" placeholder="Email or phone number" onChange={handleChange}/>
+            {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+            <Input name="password" placeholder="Password" onChange={handleChange}/>
+             {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
+            <Button disabled={!isValid}>Sign In</Button>
+        </Form>
+        </Hero>
     )
 }
 
